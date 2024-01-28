af3b30a501ac243bb95002bbce9fac3e3bb64be5
mkc-c <abdirisak.arr@gmail.com>
2024-01-28 05:16:56 +0000

    remove sets from contructor parameters, initialise sets fields to hashset

--- src/main/java/guru/spring/springapplication/model/Author.java
+++ src/main/java/guru/spring/springapplication/model/Author.java
@@ -2,6 +2,7 @@ package guru.spring.springapplication.model;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -13,15 +14,14 @@ public class Author {
     private String firstName;
     private String lastName;
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<>();
 
     public Author() {
     }
 
-    public Author(String firstName, String lastName, Set<Book> books) {
+    public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.books = books;
     }
 
     public Long getId() {
