fbffd272c07f29e7b5bd7a37bf428fd215b25ff3
mkc-c <abdirisak.arr@gmail.com>
2024-01-28 05:17:58 +0000

    create bootstrap commandline runner bean

---
+++ src/main/java/guru/spring/springapplication/bootstrap/BootStrapData.java
@@ -0,0 +1,43 @@
+package guru.spring.springapplication.bootstrap;
+
+import guru.spring.springapplication.model.Author;
+import guru.spring.springapplication.model.Book;
+import guru.spring.springapplication.repositories.AuthorRepository;
+import guru.spring.springapplication.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Development", "1234");
+
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "4321");
+
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of books: " + bookRepository.count());
+    }
+}
