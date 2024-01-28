9aeafecd0307dc9875c50425054d78d1951eec32
mkc-c <abdirisak.arr@gmail.com>
2024-01-28 05:20:32 +0000

    change interfaces to extend CrudRepository

--- src/main/java/guru/spring/springapplication/repositories/AuthorRepository.java
+++ src/main/java/guru/spring/springapplication/repositories/AuthorRepository.java
@@ -1,2 +1,7 @@
-package guru.spring.springapplication.repositories;public interface AuthorRepository {
+package guru.spring.springapplication.repositories;
+
+import guru.spring.springapplication.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
--- src/main/java/guru/spring/springapplication/repositories/BookRepository.java
+++ src/main/java/guru/spring/springapplication/repositories/BookRepository.java
@@ -1,2 +1,7 @@
-package guru.spring.springapplication.repositories;public interface BookRepository {
+package guru.spring.springapplication.repositories;
+
+import guru.spring.springapplication.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
 }
