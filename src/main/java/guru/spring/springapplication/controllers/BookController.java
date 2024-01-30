1d673aeaca7ba34efcbd784d489707f463d30c63
mkc-c <abdirisak.arr@gmail.com>
2024-01-30 10:22:50 +0000

    create and add book contoller

---
+++ src/main/java/guru/spring/springapplication/controllers/BookController.java
@@ -0,0 +1,24 @@
+package guru.spring.springapplication.controllers;
+
+import guru.spring.springapplication.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+}
